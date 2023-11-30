727ec450498e37ca4e9b505c455e9de6ac68aa34
keverson augusto dias da silva <140671157+BIBIKEV42@users.noreply.github.com>
2023-11-30 15:40:38 -0300

    Add files via upload

--- connection.js
+++ connection.js
@@ -1,17 +1,18 @@
 const Sequelize = require('sequelize');
-//CONEXÃO COM DB MYSQL
+
+// CONEXÃO COM O BANCO DE DADOS MYSQL
 const DB = new Sequelize("mksolucoes", "root", "1234", {
     host: 'localhost',
     dialect: 'mysql'
 });
 
-module.exports = DB;
-//RETORNAR SE TEVE SUCESSO NA CONEXÃO
-DB.authenticate();
-/*.then(function(){
-    console.log("Conexão realizada com sucesso!")
-}).catch(function(){
-    console.log("Erro de conexão!")
-});*/
+// RETORNAR SE TEVE SUCESSO NA CONEXÃO
+DB.authenticate()
+    .then(function () {
+        console.log("Conexão realizada com sucesso!");
+    })
+    .catch(function (err) {
+        console.error("Erro de conexão:", err);
+    });
 
-module.exports = DB, Sequelize;
+module.exports = { DB, Sequelize };
