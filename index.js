1941954009b39ce1bc1827922b2d898159681b8c
MaxwellVBr <151460628+MaxwellVBr@users.noreply.github.com>
2023-11-30 21:39:22 -0300

    Update index.js
    Atualizado a rota /pedido

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-const Pedidos = require("./DAO/PedidosDAO")
-
-//Express
 const express = require('express');
-const Login = require('./DAO/LoginDAO')
 const app = express();
+const Pedidos = require("./DAO/PedidosDAO");
 
 // BodyParser
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Config Template Engine Handlebars
-const handlebars = require('express-handlebars')
-app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-app.set('view engine', 'handlebars')
+const handlebars = require('express-handlebars');
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-
-
-app.get('/pedido', function(req, res){
+app.get('/pedido', function(req, res) {
     res.render('pedido');
-})
+});
 
-app.post('/pedido', function(req, res){
+app.post('/cadpedido', function(req, res) {
     Pedidos.create({
         Nome: req.body.nomeCliente,
         Endereco: req.body.enderecoCliente,
@@ -30,31 +24,32 @@ app.post('/pedido', function(req, res){
         Produto: req.body.produtoCliente,
         Quantidadem2: req.body.quantidadeProduto,
         ModoTelha: req.body.modoTelha
-        // Adicione outras propriedades ao objeto conforme necessário
-    }).then(function() {
+        // Adicionar outras propriedades ao objeto conforme for necessário
+    })
+    .then(function() {
         res.redirect('pedido');
     })
     .catch(function(erro) {
+        console.error("Falha:", erro);
         res.status(500).json({ erro: "Falha: " + erro });
     });
-    
-})
+});
 
-/*app.get('/login', function(req, res){
+/*app.get('/login', function(req, res) {
     res.render('login');
-})
+});
 
-app.post('/login', function(req, res){
+app.post('/login', function(req, res) {
     if (req.body.username == "usuario" && req.body.password == "senha") {
         res.redirect('test');
     } else {
-        
-    } 
-})*/
+        // Lógica para lidar com login inválido
+    }
+});*/
 
 //req.flash("success_msg", "Pedido gerado com sucesso!")
 
-//localhost:8080
-app.listen(8080, function(){
+// localhost:8080
+app.listen(8080, function() {
     console.log("Servidor Rodando");
-    });
+});
